cb0890459dd8120ed55be3ad8eee391f25291b28
midoghranek <midoghranek@hotmail.com>
2021-12-29 13:35:50 +0200

    fix data dir folder in productionr

--- shared/utils/constants/index.ts
+++ shared/utils/constants/index.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_LANG = "default";
 export const COOKIE_LANG = "COOKIE_LANG";
 export const PUBLIC_DATA_DIR =
-  process.env.NODE_ENV === "production" ? "./public/data" : "./data";
+  process.env.NODE_ENV === "production" ? "./data" : "./public/data";
